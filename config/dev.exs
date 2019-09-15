fc147725389b55ab9a714a4035f2d9f88acf1528
JunrillG <junrillg@icloud.com>
2019-09-15 21:35:16 +0800

    fix npm audit and update dev config

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :uLoan, ULoan.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   database: "uloan_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
